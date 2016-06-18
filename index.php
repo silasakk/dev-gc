3a8331d6d825fa674e7c42b7075c9816fa39d75f
Silasak Lawong <silasak.lf@gmail.com>
2016-06-18 10:46:11 +0700

    upload

---
+++ index.php
@@ -0,0 +1,230 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>GOLD COSMETIC</title>
+
+    <!-- Bootstrap -->
+    <link href="assets/lib/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Fontawesome -->
+    <link href="assets/lib/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+
+    <!-- FLEXSLIDER -->
+    <link href="assets/lib/flexslider/flexslider.css" rel="stylesheet">
+
+    <!-- Main -->
+    <link href="assets/css/main.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<div class="header">
+    <div class="container">
+        <div class="logo">
+            <img src="assets/images/logo.png" alt="">
+        </div>
+
+        <div class="nav-menu">
+            <ul class="list-unstyled">
+                <li class="active"><a href="">หน้าหลัก</a></li>
+                <li><a href="">ตัวแทนจำหน่าย</a></li>
+                <li><a href="">รีวิว</a></li>
+                <li><a href="">ติดต่อเรา</a></li>
+            </ul>
+        </div>
+        <div onclick="$('.nav-menu').slideToggle('fast  ')" class="menu-mobile">
+            <i class="fa fa-bars"></i>
+        </div>
+    </div>
+</div>
+
+<div class="banner-slider">
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+            <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+            <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+        </ol>
+
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner" role="listbox">
+
+            <div class="item active">
+                <img src="assets/images/banner-cosmetic.jpg" class="img-responsive">
+            </div>
+            <div class="item">
+                <img src="assets/images/banner-cosmetic.jpg" class="img-responsive">
+            </div>
+            <div class="item">
+                <img src="assets/images/banner-cosmetic.jpg" class="img-responsive">
+            </div>
+
+        </div>
+
+    </div>
+</div>
+<section class="hl">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+                <div class="about pt60">
+                    <div class="txt-about">เกี่ยวกับเรา</div>
+                    <h1>" Gold Glass Mask "</h1>
+                    <p>
+                        เป็นครีมเจ้าแรกในประเทศไทยที่รวมสิริมงคลไว้ในกระปุกเดียว ไม่เชื่อก็ต้องเชื่อนะคะลอตแรกปล่อยออกไปถูกหวยกันแล้ว ปังงง!!
+                        (รุ่นรวยโคตร รอบนี้นี้อุ้มสั่งพระอาจารย์ให้เสกมา100,000 กระปุก นะคะสำหรับตัวแทน เปิดมาเจอแผ่นทองลงยันต์ทุกปุก Firm!
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-6 ">
+                <div class="about text-center">
+                    <img src="assets/images/coin.png" class="img-responsive">
+                    <h1>“ ไม่เชื่อก็ต้องเชื่อ ”</h1>
+                    <p>ลอตแรกปล่อยออกไปถูกหวยกันแล้วว ปัง!!</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="hl">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-6">
+
+                    <img src="assets/images/pre1.png" class="img-responsive spec1" alt="">
+
+            </div>
+            <div class="col-sm-6">
+                <div class="about">
+                    <div class="txt-about">
+                        สินค้า ของเรา
+                    </div>
+                    <p>
+                        ผลิตมาจาก. สารสกัดผลทับทิมธรรมชาติ (สั่งนำเข้าจากประเทศ
+                        ญี่ปุ่นโดยเฉพาะ) ซึ่งให้อนุพันธ์ วิตามินเอ ซี อี สูงถึง4 เท่า ของผลไม้ รสเปรี้ยวทั่วไปช่วยให้ผิวหน้ากระจ่างใสในชั่วข้ามคืน
+
+                    </p>
+                    <img src="assets/images/gra.png" class="img-responsive" alt="">
+                    <p>
+                        โดยไม่เป็นอันตรายและไม่มีสารตกค้าง เมื่อนวดไปสักระยะ จะเกิดการดีท้อกซ์ของเสียออกจากหน้าซึ่งตัวผู้ใช้จะรู้สึกถึงไออุ่นๆ ร้อน วูบวาบ สักพักจะหายไป
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="hl">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-7">
+                <div class="about">
+                    <div class="txt-about">
+                        ตัวแทนจำหน่าย
+                    </div>
+                    <p>
+                        เปิดรับสมัครตัวแทนขาย ระดับVVIP ,VIP ,VP และMember รับจำนวนจำกัด สะสมคะแนน รับของรางวัล
+                    </p>
+                    <p>
+                        สนใจสวย และรวยไวๆๆ รีบทักมานะคะline id : @russarin_brand
+                        ไม่ต้องทักinboxอุ้มมาน่ะค่า)
+                        ตอนนี้ตัวแทนภาค เหนือ อีสาน กลาง ใต้ ตะวันออก ตะวันตก กรุงเทพ มีตัวแทนครบแล้ว.. บางจังหวัดถูกจองไปเรียบร้อย
+                        เด่วเราจะเริ่มปฏิบัติการสวยและรวยจิงจังกันแล้วนะคะ
+                        ขอบคุณที่ให้ความสนใจค่ะ
+                    </p>
+                </div>
+            </div>
+            <div class="col-sm-5">
+                <img src="assets/images/pre2.png" style="margin-bottom: 0" class="img-responsive" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+<section class="hl-review">
+    <div class="container">
+
+
+        <div class="row">
+            <div class="txt-head">
+                รีวิว จากลูกค้าของเรา
+            </div>
+
+
+            <div class="col-sm-4 no-padding">
+                <img src="assets/images/test/rw1.jpg" class="img-responsive" alt="">
+            </div>
+            <div class="col-sm-4 no-padding">
+                <img src="assets/images/test/rw2.jpg" class="img-responsive" alt="">
+            </div>
+            <div class="col-sm-4 no-padding">
+                <img src="assets/images/test/rw3.jpg" class="img-responsive" alt="">
+            </div>
+            <div class="col-sm-4 no-padding">
+                <img src="assets/images/test/rw4.jpg" class="img-responsive" alt="">
+            </div>
+            <div class="col-sm-4 no-padding">
+                <img src="assets/images/logo-rw.png" class="img-responsive" alt="">
+            </div>
+            <div class="col-sm-4 no-padding">
+                <img src="assets/images/test/rw6.png" class="img-responsive" alt="">
+            </div>
+            <div class="col-sm-4 no-padding">
+                <img src="assets/images/test/rw7.jpg" class="img-responsive" alt="">
+            </div>
+            <div class="col-sm-4 no-padding">
+                <img src="assets/images/test/rw8.jpg" class="img-responsive" alt="">
+            </div>
+            <div class="col-sm-4 no-padding">
+                <img src="assets/images/test/rw9.jpg" class="img-responsive" alt="">
+            </div>
+        </div>
+    </div>
+</section>
+
+<footer>
+    <a href="" class="btt">
+        BACK TO TOP <i class="fa fa-angle-up" aria-hidden="true"></i>
+    </a>
+    <div class="container">
+        <div class="social">
+            <a href="">
+                <img src="assets/images/ico-facebook.png" alt="">
+            </a>
+            <a href="">
+                <img src="assets/images/ico-twitter.png" alt="">
+            </a>
+            <a href="">
+                <img src="assets/images/ico-google.png" alt="">
+            </a>
+            <a href="">
+                <img src="assets/images/ico-youtube.png" alt="">
+            </a>
+            <a href="">
+                <img src="assets/images/ico-line.png" alt="">
+            </a>
+
+        </div>
+        <div class="copyright">
+            WWW.GOLDGLASSMASKBYNATTAYUI.CO.TH TEL.098-9450045
+        </div>
+    </div>
+</footer>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="assets/lib/bootstrap/js/bootstrap.min.js"></script>
+<script src="assets/lib/flexslider/jquery.flexslider-min.js"></script>
+</body>
+</html>
